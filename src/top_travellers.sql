69416fd1bae28263e1ad3d54d583e9b8fd54eb19
Mike Kalvas <michaelkalvas@loopreturns.com>
2026-08-31 10:50:46 -0400

    feat: leetcode #1407 - easy - top-travellers

---
+++ src/top_travellers.sql
@@ -0,0 +1,10 @@
+# id=1541 slug=top-travellers lang=mysql
+
+# Write your MySQL query statement below
+select
+  u.name,
+  coalesce(sum(r.distance), 0) as travelled_distance
+from
+  Users u left join Rides r on u.id = r.user_id
+group by u.id
+order by travelled_distance desc, u.name asc;
